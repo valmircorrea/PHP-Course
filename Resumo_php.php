4694b28746872209e9a75fa0cb547d71135a1b32
Valmir Correa <valmir.correa@ufrn.edu.br>
2017-12-10 12:09:29 -0300

    Primeiros codigos em PHP

---
+++ Resumo_php.php
@@ -0,0 +1,13 @@
+<?php // tudo vai entre essas tags
+
+$variavel_1 = 'É assim que se define uma variavel em php';
+
+/* não se deve definir variaveis assim:
+
+$20_anos_e_nome = 'Mauro George';
+$ nascimento = '06/09/1989';
+$*este_é_meu_sobrenome = 'Oliveira Tavares';
+
+*/
+
+?>
